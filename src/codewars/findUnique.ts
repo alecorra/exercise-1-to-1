352cc53d6303bc360482691e75c95cc0b91a5b11
alessandro-corra <alessandro.corra@2132.io>
2023-04-24 22:07:01 +0100

    find unique mnumber - solution

--- src/codewars/findUnique.ts
+++ src/codewars/findUnique.ts
@@ -1,21 +1,23 @@
 // length = 20
 const input = [1, 1, 1, 1, 2, 2, 2, 2, 5, 5, 5, 5, 7, 7, 7, 7, 8, 9, 9, 9];
 
-const findUniques = (numberArray: number[]): any => {
+const findUniques = (numberArray: number[]): number => {
   const valueToChcek = numberArray[0];
   let newArray: number[] = [];
 
   for (let index = 1; index < numberArray.length; index++) {
     if (valueToChcek === numberArray[index]) {
       newArray = numberArray.filter((value) => value !== valueToChcek);
-
       break;
     }
+
+    newArray = [valueToChcek];
   }
+
   if (newArray.length > 1) {
     return findUniques(newArray);
   }
-  console.log(newArray);
+
   return newArray[0];
 };
 
